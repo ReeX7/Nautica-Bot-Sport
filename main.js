9e4577ca82ce655d4642140969c606639a688d91
ReeX7 <mendezvipz8@gmail.com>
2024-10-07 17:07:11 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `YerayBotSession`;
+global.authFile = `NauticaBotSession`;
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile);
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache()
