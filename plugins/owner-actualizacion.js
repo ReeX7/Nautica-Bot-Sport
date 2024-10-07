5309debadea63ce9fbd446625c0334193a225cd9
ReeX7 <mendezvipz8@gmail.com>
2024-10-06 21:09:50 -0600

    Actualizar owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -5,7 +5,7 @@ let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
 const owner = 'sebasmvp';
-const repo = 'YerayBot-MD';
+const repo = 'NauticaBot-MD';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: m});  
 try {
